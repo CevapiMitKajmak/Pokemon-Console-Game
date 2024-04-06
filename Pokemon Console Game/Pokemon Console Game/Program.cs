ffc55381a1d59ecc809d6c6573708f7719ffdd4d
Ahmad Becirovic <Crunch2404@hotmail.com>
2024-04-06 23:58:23 +0200

    added Project

---
+++ Pokemon Console Game/Pokemon Console Game/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Pokemon_Console_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
