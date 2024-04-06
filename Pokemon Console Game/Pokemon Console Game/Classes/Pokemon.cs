25b32f1ed7356ba3799622e1fc66ee668ccd5674
Ahmad Becirovic <Crunch2404@hotmail.com>
2024-04-07 00:30:51 +0200

    added Pokemon class and ability class

---
+++ Pokemon Console Game/Pokemon Console Game/Classes/Pokemon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon_Console_Game.Classes
+{
+    public class Pokemon
+    {
+        private Guid _id;
+        private string _name;
+        private int _level;
+        private float _health;
+
+        public Pokemon(string name, float damage, int level, float health)
+        {
+            _id = Guid.NewGuid();
+            _name = name;
+            _level = level;
+            _health = health;
+        }
+
+        public float fight(Pokemon PokemonA, Pokemon PokemonB)
+        {
+            
+        }
+    }
+}
