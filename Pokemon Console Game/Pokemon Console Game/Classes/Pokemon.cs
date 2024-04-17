7037231dfa11a548c0f17d5a197a2cc887cd5236
Ahmad Becirovic <Crunch2404@hotmail.com>
2024-04-17 21:56:18 +0200

    testing attack methode Pokemon

--- Pokemon Console Game/Pokemon Console Game/Classes/Pokemon.cs
+++ Pokemon Console Game/Pokemon Console Game/Classes/Pokemon.cs
@@ -8,22 +8,16 @@ namespace Pokemon_Console_Game.Classes
 {
     public class Pokemon
     {
-        private Guid _id;
-        private string _name;
-        private int _level;
-        private float _health;
+        public string name;
+        public double health;
+        public double damage;
 
-        public Pokemon(string name, float damage, int level, float health)
-        {
-            _id = Guid.NewGuid();
-            _name = name;
-            _level = level;
-            _health = health;
-        }
 
-        public float fight(Pokemon PokemonA, Pokemon PokemonB)
+       public static double Attack(double damage, Pokemon a)
         {
-            
+            a.health -= damage;
+                
+                return a.health;
         }
     }
 }
--- Pokemon Console Game/Pokemon Console Game/Program.cs
+++ Pokemon Console Game/Pokemon Console Game/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Pokemon_Console_Game
+﻿using Pokemon_Console_Game.Classes;
+using static Pokemon_Console_Game.Classes.Pokemon;
+
+namespace Pokemon_Console_Game
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Pokemon Charizard = new Pokemon();
+
+            Charizard.name = "Charizard";
+            Charizard.health = 100;
+            Charizard.damage = 10;
+
+            Pokemon Bulbasaurus = new Pokemon();
+
+            Bulbasaurus.name = "Bulbasaurus";
+            Bulbasaurus.health = 100;
+            Bulbasaurus.damage = 10;
+
+            Attack(Bulbasaurus.damage,Charizard);
+
+            Console.WriteLine(Charizard.health);
         }
     }
 }
